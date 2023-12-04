b21f53a1f202ecd656e0c2824dba660c9cbf1325
Jonas Pauwels <jonas.pauwels@student.hogent.be>
2023-12-04 13:05:52 +0100

    altered custom config file

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -2,17 +2,11 @@ module.exports = {
     env: 'NODE_ENV',
     port: 'PORT',
     database: {
-      // host: 'DATABASE_HOST',
-      // port: 'DATABASE_PORT',
-      // name: 'DATABASE_NAME',
-      // username: 'DATABASE_USERNAME',
-      // password: process.env.DATABASE_PASSWORD,
-      client: process.env.DATABASE_CLIENT,
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      name: process.env.DATABASE_NAME,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD
+      host: 'DATABASE_HOST',
+      port: 'DATABASE_PORT',
+      name: 'DATABASE_NAME',
+      username: 'DATABASE_USERNAME',
+      password: process.env.DATABASE_PASSWORD,
     },
     auth: {
       jwt: {
